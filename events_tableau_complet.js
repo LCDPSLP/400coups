2ae2ae325d7e4f7ee2a716313a1df83e08bfdf5a
Les400coups <les400coups78@gmail.com>
2025-05-26 19:50:07 +0200

    ajout

---
+++ events_tableau_complet.js
@@ -0,0 +1,76 @@
+
+const events_tableau = [
+
+/*
+  <span style='color:red'> pour la couleur
+  <br> saut de ligne
+ <a href='https://www.leboucanier.net' target='_blank'>www.leboucanier.net</a> pour lien direct
+*/
+
+
+  {
+    id: "20-10-2025",
+    titre: "Les Automnales – Vernouillet",
+    date: "20 octobre 2025",
+    date_iso: "2025-10-20",
+    lieu: "Parc des Buissons, Vernouillet",
+    description: "Un spectacle d'automne où l'improvisation chante la saison et vos envies.",
+    affiche: "images/affiche_400_coups.png",
+        highlight: false
+
+  },
+  {
+    id: "27-06-2025",
+    titre: "Les 400 Coups au Boucanier",
+    date: "27 juin 2025 à 21h",
+    date_iso: "2025-06-27",
+    lieu: "<span style='color:red'>Le Boucanier</span>  <br>Allée des Iles Tabarly <br>78200 Mantes-la-Jolie. <br>Restaurant dès 19h30. <br>Réservation :<br> <a href='https://www.leboucanier.net' target='_blank'>www.leboucanier.net</a><br> 01.30.63.70.30",
+    description: "Improviser, c'est possible. <br>Chanter, c'est possible. <br>Mais improviser en chantant ? <br> Bah c'est possible !",
+    affiche: "images/affiche_27_06_2025.jpg",
+        highlight: true
+  },
+  
+  {
+    id: "16-06-2024",
+    titre: "Péniche de Triel",
+    date: "16 juin 2024 à 21h",
+    date_iso: "2024-06-16",
+    lieu: "LaPéniche de Triel, 69 Quai Auguste Roy 78 Triel sur Seine",
+    description: "Un spectacle remplie d'eau.",
+    affiche: "images/affiche_16_06_2024.jpg",
+        highlight: false
+  },
+  
+  {
+    id: "26-05-2024",
+    titre: "Stage de chant improvisé",
+    date: "26 Mai 2024 à 16h",
+    date_iso: "2024-05-26",
+    lieu: "La Péniche de Triel, 69 Quai Auguste Roy 78 Triel sur Seine",
+    description: "apprendre à inventer et chanter une chanson improvisée: paroles, interprétation, mélodie...",
+    affiche: "images/affiche_26_05_2024.jpg",
+        highlight: false
+  },
+  
+  
+  {
+    id: "25-05-2024",
+    titre: "Chante ton impro",
+    date: "25 Mai 2024 à 21h",
+    date_iso: "2024-05-25",
+    lieu: "Espace Bosco (EDMA) Boulevard de l'Europe 78540 Vernouillet",
+    description: "Chansons chantés sans son chien",
+    affiche: "images/affiche_16_06_2024.jpg",
+        highlight: false
+  }
+  
+  
+  
+  
+  
+  
+  
+];
+
+
+window.events_tableau = events_tableau;
