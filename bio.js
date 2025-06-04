fd6baa91a909a40a7cfb1165511aacfef45ede4c
Les400coups <les400coups78@gmail.com>
2025-06-04 12:58:17 +0200

    250604d

---
+++ bio.js
@@ -0,0 +1,7 @@
+const bios = {
+  noemie: "Elle chante, elle improvise, et surtout, elle refuse de choisir : elle improvise en chantant, tout simplement.",
+  lilou: "Avec son cerveau en freestyle, elle découvre ce qu’elle chante en même temps que vous. Suspense et surprises garantis !",
+  eric: "Il ignore ce qu’il va chanter. Son cerveau improvise en douce pendant que sa bouche fait semblant de savoir...",
+  ericl: "Tout le monde aime les pommes de terre",
+  camille: "MC professionnel à l'humour exceptionnel"
+};
